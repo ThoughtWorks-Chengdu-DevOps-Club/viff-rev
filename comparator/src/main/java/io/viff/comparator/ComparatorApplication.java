3cdf46d42a38605c45dff00f8bbce61e333e4c56
Tianbo Zhang <ztbsuper@gmail.com>
2016-04-05 10:19:19 +0800

    [tianbo] add feign dependency

--- comparator/src/main/java/io/viff/comparator/ComparatorApplication.java
+++ comparator/src/main/java/io/viff/comparator/ComparatorApplication.java
@@ -2,13 +2,15 @@ package io.viff.comparator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@EnableEurekaClient
 public class ComparatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComparatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ComparatorApplication.class, args);
+    }
 }
--- storage/src/main/java/io/viff/storage/StorageApplication.java
+++ storage/src/main/java/io/viff/storage/StorageApplication.java
@@ -2,11 +2,12 @@ package io.viff.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StorageApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(StorageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApplication.class, args);
+    }
 }
