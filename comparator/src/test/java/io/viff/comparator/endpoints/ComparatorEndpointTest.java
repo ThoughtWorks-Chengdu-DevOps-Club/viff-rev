daa72a4c28774f3ead3aebb75b70e767646827be
Tianbo Zhang <ztbsuper@gmail.com>
2016-04-08 16:04:45 +0800

    [issue-6] fix build

--- comparator/src/test/java/io/viff/comparator/endpoints/ComparatorEndpointTest.java
+++ comparator/src/test/java/io/viff/comparator/endpoints/ComparatorEndpointTest.java
@@ -2,25 +2,19 @@ package io.viff.comparator.endpoints;
 
 
 import io.viff.comparator.ComparatorApplication;
-import io.viff.comparator.domain.CompareResult;
-import io.viff.comparator.domain.UrlStorage;
-import jersey.repackaged.com.google.common.collect.Lists;
+import io.viff.sdk.response.CompareResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.http.*;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -44,7 +38,7 @@ public class ComparatorEndpointTest {
         argMap.put("to", "http://img1.bdstatic.com/static/home/widget/search_box_home/logo/home_white_logo_0ddf152.png");
         argMap.put("port", String.valueOf(port));
 
-        ResponseEntity<CompareResult> responseEntity = restTemplate.getForEntity("http://localhost:{port}/compare?from={from}&to={to}",CompareResult.class, argMap);
+        ResponseEntity<CompareResult> responseEntity = restTemplate.getForEntity("http://localhost:{port}/compare?from={from}&to={to}", CompareResult.class, argMap);
 
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
         CompareResult compareResult = responseEntity.getBody();
